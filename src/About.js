9a5e26640a20f44568c51bd45dd41c99849f6775
Jaiveer <jaiveerdhanju@hotmail.com>
2022-01-05 21:16:43 -0800

    wip

--- src/About.js
+++ src/About.js
@@ -1,8 +1,14 @@
+import face from './images/face.png'
+
 function About(){
 
     return(
-        <div>
-            <p>"In About"</p>
+        <div className='aboutContent'>
+            <img src={face} className='myFace'/>
+            <p>This is just a test. I just want to see what happens when I add alot of text for my bio
+                please don't mess up the layout. Time for some random text: asdf  sdfsadfsadf s df sadf as df
+                sadfsadfsaf  sdf as  asdfsdafas fsafd asdf  sadfasdfsadfsadf sadfsadfsad.
+            </p>
         </div>
     );
 }
--- src/Modals/rainforestModel.js
+++ src/Modals/rainforestModel.js
@@ -4,14 +4,35 @@ function RainforestModal(props){
     return(
         <Modal show={props.modalShow} onHide = {props.modalHide}>
             <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Rainforest Automation</Modal.Title>
             </Modal.Header>
-            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-            <Button variant="secondary" onClick={() => props.modalHide()}>
-                Close
-            </Button>
+
+            <Modal.Body style={{ fontWeight: 'bold' }}>Duration: May 2021 - Dec 2021 (8 Months)</Modal.Body>
+
+            <Modal.Body>
+                <ul>
+                    <li>
+                        Developed an engagement portal for Puget Sound Energy using React, Redux, AWS services, OAuth 2.0, and a content management system.
+                    </li>
+                    <br />
+                    <li>
+                        Implemented new features for the company's user portal using React and Redux.
+                    </li>
+                    <br />
+                    <li>
+                        Maintained, updated, and debugged 40+ tickets for React components, Redux stores, and optimized REST API request functions.
+                    </li>
+                    <br />
+                    <li>
+                        Troubleshooted UNIX firmware using bash scripts and XML commands.
+                    </li>
+                </ul>
+            </Modal.Body>
+
             <Modal.Footer>
-                Close
+                <Button color="secondary" onClick={() => props.modalHide()}>
+                    Close
+                </Button>    
             </Modal.Footer>
       </Modal>
     );
